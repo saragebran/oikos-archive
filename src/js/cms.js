069f023b7a10c230714df1e995008bbe5f0ebd44
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2024-03-21 18:47:01 +0000

    updates

--- src/js/cms.js
+++ src/js/cms.js
@@ -5,26 +5,69 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import SpeciesPreview from "./cms-preview-templates/species";
 
-CMS.registerEventListener({
-    name: 'mounted',
-    handler: () => {
-      const pathSegments = window.location.pathname.split('/');
-      const newSegmentIndex = pathSegments.indexOf('new');
-      
-      // Check if there is a segment after "new/"
-      if (newSegmentIndex >= 0 && pathSegments.length > newSegmentIndex + 1) {
-        const uuid = pathSegments[newSegmentIndex + 1]; // Get the UUID from the URL
-        
-        // Now, find the input field for speciesid and populate it with the UUID
-        const inputField = document.querySelector('[name="fields[speciesid]"]');
-        if (inputField) {
-          inputField.value = uuid;
-        }
+ CMS.registerEventListener({
+    name: 'login',
+    handler: ({ author: { login, name } }) => {
+      const hashSegments = window.location.hash.split('/');
+      const sciname = hashSegments[4];
+      if (sciname) {
+        setTimeout(() => {
+            function simulateKeyPress(element, key) {
+                // Create and dispatch the keydown event
+                const keydownEvent = new KeyboardEvent('keydown', { key: key, bubbles: true, cancelable: true });
+                element.dispatchEvent(keydownEvent);
+            
+                // Create and dispatch the keyup event
+                const keyupEvent = new KeyboardEvent('keyup', { key: key, bubbles: true, cancelable: true });
+                element.dispatchEvent(keyupEvent);
+            }
+            function simulateTyping(inputField, text, callback) {
+                inputField.focus();  // Focus the field to start
+              
+                text.split('').forEach((char, index) => {
+                  setTimeout(() => {
+                    // Create and dispatch a keydown event
+                    const keydownEvent = new KeyboardEvent('keydown', { key: char, bubbles: true, cancelable: true });
+                    inputField.dispatchEvent(keydownEvent);
+              
+                    // Change the value and dispatch an input event
+                    inputField.value += char;
+                    const inputEvent = new Event('input', { bubbles: true });
+                    inputField.dispatchEvent(inputEvent);
+              
+                    // Create and dispatch a keyup event
+                    const keyupEvent = new KeyboardEvent('keyup', { key: char, bubbles: true, cancelable: true });
+                    inputField.dispatchEvent(keyupEvent);
+              
+                    // If the last character is typed, execute the callback
+                    if (index === text.length - 1) {
+                      setTimeout(callback, 500); // Adjust delay as necessary to allow suggestions to appear
+                      // Simulate pressing the arrow down key
+                      simulateKeyPress(inputField, 'ArrowDown');
+                                          
+                      // Simulate pressing the 'Enter' key
+                      simulateKeyPress(inputField, 'Enter');
+                    }
+                  }, index * 100); // Delay to simulate typing speed
+                });
+              }
+              const inputField = document.getElementById(':r2:');
+              simulateTyping(inputField, sciname, () => {
+                // Callback function to execute after typing is complete
+                console.log('Typing complete');
+                const button = document.querySelector('button[data-testid="choose-upload"]');
+                if (button) {
+                  button.focus();
+                }            
+              }); 
+        }, 2000); // Adjust the timeout duration based on how long it takes for the options to appear
       }
     },
-  });
+  }); 
+  
+  
   
 
 CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("post", SpeciesPreview);
+CMS.registerPreviewTemplate("species", SpeciesPreview);
 CMS.init();
