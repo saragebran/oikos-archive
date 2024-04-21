65b67fee1216d0782626ed0338ebd27dceb1c724
jazbogross <57679104+jazbogross@users.noreply.github.com>
2024-04-21 14:21:22 +0100

    preSave event

--- src/js/cms.js
+++ src/js/cms.js
@@ -7,9 +7,31 @@ import SpeciesPreview from "./cms-preview-templates/species";
 import RepresentationsPreview from "./cms-preview-templates/representations";
 
 
+  // Listen for ALL preSave events
+  CMS.registerEventListener({
+    name: 'preSave',
+    collection: 'species',
+    handler: ({ entry }) => {
+        if (entry && entry.data) {
+            const uuid = entry.data.uuid;  // Accessing UUID from the nested data
+            // Update the slug and path within the nested entry data
+            const updatedEntryData = {
+                ...entry.data,
+                slug: uuid,
+                path: `site/content/species/${uuid}/_index.md`
+            };
+
+            // Return the updated entry object at the top level
+            return {
+                ...entry,
+                data: updatedEntryData
+            };
+        }
+        return entry;  // Return unchanged entry if conditions aren't met
+    }
+});
   
-  
-  
+
   
 
 CMS.registerPreviewStyle(styles, { raw: true });
