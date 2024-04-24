5aa97134a9fb08cea7c6ed98bdff6ccecc99520a
jazbogross <57679104+jazbogross@users.noreply.github.com>
2024-04-24 20:13:51 +0100

    gallery

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,29 +6,54 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 import SpeciesPreview from "./cms-preview-templates/species";
 import RepresentationsPreview from "./cms-preview-templates/representations";
 
+CMS.registerEventListener({
+  name: 'login',
+  handler: () => {
+    setTimeout(() => {
+      const speciesInput = document.querySelector('.CMS_Autocomplete_input');
+      speciesInput.focus();
+      // Clear existing value and trigger update
+      speciesInput.value = '';
+      speciesInput.dispatchEvent(new Event('input', { bubbles: true })); // Reset the component's internal state
+      speciesInput.dispatchEvent(new Event('change', { bubbles: true }));
+
+      setTimeout(() => {
+        speciesInput.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true, key: 'ArrowDown' }));
+        setTimeout(() => {
+          speciesInput.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true, key: 'Enter' }));
+        }, 500); // Short delay before pressing 'Enter'
+
+      }, 500); // Ensure all typing completes before navigating
+    }, 2000);
+  }
+});
 
 CMS.registerEventListener({
-    name: 'mounted',
-    handler: () => {
-      // Only execute this script if the URL indicates that a new 'representations' entry is being created
-      if (window.location.pathname.includes('/representations/new')) {
-        // Use setTimeout to ensure the DOM is fully loaded
-        setTimeout(() => {
-          // Assume the URL contains a parameter like ?speciesid=UUID
-          const urlParams = new URLSearchParams(window.location.search);
-          const speciesId = urlParams.get('speciesid'); // This should match the query parameter in the URL
-          
-          // Find the input field for 'speciesid' in the CMS form
-          const speciesIdInput = document.querySelector('input[name="speciesid"]');
-          
-          if (speciesIdInput && speciesId) {
-            // Set the value of the speciesid field
-            speciesIdInput.value = speciesId;
-          }
-        }, 500); // Adjust the timeout as needed based on how quickly your CMS typically loads forms
-      }
-    }
-  });
+  name: 'change',
+  collection: 'representations',
+  field: 'speciesid',
+  handler: ({ data, collection, field }) => {
+    const hash = window.location.hash;
+    const queryIndex = hash.indexOf('?');
+    const queryParams = queryIndex !== -1 ? hash.substring(queryIndex + 1) : '';
+    const urlParams = new URLSearchParams(queryParams);
+    const speciesName = urlParams.get('speciesId');
+    const currentValue = speciesName;
+    console.log("Species ID changed to:", currentValue);
+    
+    // You can modify data if needed, or just log as above
+    return {
+      ...data
+      , speciesid: currentValue
+    };
+  },
+});
+
+
+
+
+
+
   
   
   
